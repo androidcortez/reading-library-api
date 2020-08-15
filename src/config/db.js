8c7a1a7e34fd48954dcfa97d9889d8dccf18dc68
androidcortez <androidcortez@gmail.com>
2020-08-15 12:16:55 -0500

    change connection to pool

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,12 @@
-var mysql = require('mysql');
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    dateStrings: process.env.DB_DATE_STRING,
-    database: process.env.DB_DATABASE,
-    connectTimeout: 30000
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dateStrings: process.env.DB_DATE_STRING,
+  database: process.env.DB_DATABASE,
+  connectTimeout: 30000,
 });
 
-connection.connect((err) => {
-    if (err) throw err.stack;
-    console.log("Database Connected");
-});
-
-module.exports = connection;
+module.exports = pool;
